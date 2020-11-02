38b5c02266fc733d8ecd6bec598482fe5eb774cb
Paruta Yannis <yannis.paruta@gmail.com>
2020-11-02 17:35:57 +0100

    add book vue

--- src/components/BookBookDetails.vue
+++ src/components/BookBookDetails.vue
@@ -1,16 +1,58 @@
 <template>
   <div class="row">
     <div class="col">
+      <div >
+          <section class="page-section">
+            <div class="container">
+              <div class="about-heading-content">
+                <div class="row">
+                  <div class="col-xl-9 col-lg-10 mx-auto">
+                    <div class="bg-faded rounded p-5">
+                        <div class="media">
+                          <div class="media-body text-left ml-3">
+                            <img :src="book.coverUri" :alt="book.title" />
+                            <h3 class="mt-0">{{selectedBook.title}}</h3>
+                            <h3 class="mt-0">{{selectedBook.author}}</h3>
+                            <h3 class="mt-0">{{selectedBook.year}}</h3>
+                                {{selectedBook.resume}}
+                          </div>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'BookBookDetails',
-  props: {
+  import axios from "axios";
+
+  const BOOKS_API_ENDPOINT = 'http://localhost:3000/books/';
+
+  export default {
+    name: 'bookDetails',
+    props: {
+    },
+    data:()=>({
+        allBooks: [],
+        selectedBook:{}
+    }),
+    methods:{
+      async fetchAllBooks(){
+        let allBooks = await axios.get(BOOKS_API_ENDPOINT);
+        let { data } = allBooks;
+        this.allBooks = data;
+      },
+    },
+    async created(){
+      let { bookId }    = this.$route.params;
+      this.selectedBook = this.book.find(book => book.id==bookId);
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/components/BookBooks.vue
+++ src/components/BookBooks.vue
@@ -1,14 +1,55 @@
 <template>
   <div class="row">
     <div class="col">
+
+      <table class="table table-dark">
+        <thead>
+          <tr>
+            <th scope="col">Couverture</th>
+            <th scope="col">Titre</th>
+            <th scope="col">Autheur</th>
+            <th scope="col">Année</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(book) in allBooks" :key='book.id'>
+            <td><img :src="book.coverUri" :alt="book.title" /></td>
+            <td>{{book.title}}</td>
+            <td>{{book.author}}</td>
+            <td>{{book.year}}</td>
+            <td><button @click="goToBooksDetails(book.id)" type="button" class="btn btn-secondary btn-sm">voir</button></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
+const BOOKS_API_ENDPOINT = 'http://localhost:3000/books/';
+
 export default {
   name: 'BookBooks',
   props: {
+  },
+  data:()=>({
+      allBooks: [],
+  }),
+  methods:{
+    async fetchAllBooks(){
+      let allBooks = await axios.get(BOOKS_API_ENDPOINT);
+      let { data } = allBooks;
+      this.allBooks = data;
+    },
+    goToBooksDetails(bookId){
+      this.$router.push({ name:'bookDetails', params:{bookId} })
+
+    },
+  },
+  async created(){
+    await this.fetchAllBooks();
   }
 }
 </script>
